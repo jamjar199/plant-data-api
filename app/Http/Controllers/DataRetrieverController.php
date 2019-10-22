4cddaca27c27bcb57051fe4a12bd2e85eb4e0a77
James Lilliott <jameslilliott@hotmail.com>
2019-10-22 19:39:40 +0100

    Removed unused param

--- app/Http/Controllers/DataRetrieverController.php
+++ app/Http/Controllers/DataRetrieverController.php
@@ -5,18 +5,16 @@ namespace App\Http\Controllers;
 use App\Repositories\TemperatureRepository;
 use App\Strategies\TemperatureStrategy;
 use Illuminate\Http\JsonResponse;
-use Laravel\Lumen\Http\Request;
 
 class DataRetrieverController extends Controller
 {
     /**
      * Gets the temperature data for a plant
      *
-     * @param Request $request
      * @param $id
      * @return JsonResponse
      */
-    public function getTemperature(Request $request, $id)
+    public function getTemperature($id)
     {
         $temperatureStrategy = new TemperatureStrategy(
             new TemperatureRepository()
